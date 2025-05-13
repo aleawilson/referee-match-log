33edbe353df0452e07f6b72dc3ebe196f549e298
alea-wilson <alea-wilson@yakabod.com>
2025-05-12 20:35:40 -0400

    intial setup

---
+++ src/main/java/org/andrew/refereelog/refereelog/RefereeLogApplication.java
@@ -0,0 +1,13 @@
+package org.andrew.refereelog.refereelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefereeLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RefereeLogApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/andrew/refereelog/refereelog/TestController.java
@@ -0,0 +1,12 @@
+package org.andrew.refereelog.refereelog;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
