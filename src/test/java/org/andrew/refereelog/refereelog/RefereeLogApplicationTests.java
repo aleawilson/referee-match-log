9e4b6d41ead5a7e8e971eefaa82240df556c0f70
alea-wilson <alea-wilson@yakabod.com>
2025-05-12 20:30:42 -0400

    initial setup

---
+++ src/test/java/org/andrew/refereelog/refereelog/RefereeLogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.andrew.refereelog.refereelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefereeLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
